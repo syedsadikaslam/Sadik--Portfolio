89c511aac870c1e57c841334e6e3f8d3f808e122
Md Sadik <152442843+syedsadikaslam@users.noreply.github.com>
2026-01-24 04:46:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // --------------------
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || '*',
+    origin: process.env.CLIENT_URL || '*',
   })
 );
 app.use(express.json());
