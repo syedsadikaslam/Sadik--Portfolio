6e83770f03d1a3583f1a3ecf0171d88c9e61d4bd
Md Sadik <152442843+syedsadikaslam@users.noreply.github.com>
2026-01-30 00:54:58 +0530

    Refactor Review model to use mongoose.Schema directly

--- server/models/Review.model.js
+++ server/models/Review.model.js
@@ -1,13 +1,9 @@
-// server/models/Review.model.js
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const reviewSchema = new Schema({
+const reviewSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   review: { type: String, required: true },
   rating: { type: Number, required: true, min: 1, max: 5 },
-  isApproved: { type: Boolean, default: true }, // You will manually approve reviews
+  isApproved: { type: Boolean, default: true }, 
 }, { timestamps: true });
 
-const Review = mongoose.model('Review', reviewSchema);
-module.exports = Review;
+module.exports = mongoose.model('Review', reviewSchema);
