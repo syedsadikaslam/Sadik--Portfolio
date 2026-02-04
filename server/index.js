ba0ae831629ea2ea4b9ca619eb278a46f465173f
Md Sadik <152442843+syedsadikaslam@users.noreply.github.com>
2026-02-04 19:47:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: [process.env.CLIENT_URL, "http://localhost:3000", "https://sadikaslam.vercel.app"],
+  origin: [process.env.CLIENT_URL, "http://localhost:3000", "https://sadikaslam.in"],
   credentials: true
 }));
 app.use(express.json());
